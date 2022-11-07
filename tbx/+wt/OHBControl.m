dd6beaf527cbcc079e6ef165a66d6ff90a0bd02a
Fintan Healy <qe19391@brisotl.ac.uk>
2022-11-07 15:32:17 +0000

    set windspeed + increase reliablity

--- tbx/+wt/OHBControl.m
+++ tbx/+wt/OHBControl.m
@@ -11,7 +11,13 @@ classdef OHBControl < handle
 
     methods (Access = private)
         %% Send a command to the OHB TCP server
-        function args_out = sendCmd(obj, cmd, args_in)
+        function args_out = sendCmd(obj, cmd, args_in,isStringReturn)
+            arguments
+                obj
+                cmd
+                args_in = []
+                isStringReturn = false;
+            end
             % Only continue if server is connected
             if ~obj.connected
                 disp('WARNING: OHB TCP server not connected!')
@@ -30,13 +36,30 @@ classdef OHBControl < handle
             try
                 % Send the command
                 obj.socket.write(data)
-
+%                 pause(0.5);
                 % Check for a valid response
                 data = obj.socket.readline();
+                cnt = 1;
+                while data == "" && cnt < 10
+                    pause(0.2)
+                    cnt = cnt+1;
+                    try
+                        data = obj.socket.readline();
+                    catch
+                    end
+                end
+                if cnt == 10
+                    obj.connected = false;
+                    error('No Return Message: OHB TCP server disconnected!')
+                end
                 data = split(data, ',');
                 
                 response = data{1};
-                args_out = str2double(data(2:end));
+                if ~isStringReturn
+                    args_out = str2double(data(2:end));
+                else
+                    args_out = data(2);
+                end
 
                 if strcmp(response, 'NACK')
                     % Bad response => do nothing
@@ -48,7 +71,7 @@ classdef OHBControl < handle
                     disp('WARNING: Received no response from OHB TCP server!')
                     return
                 end
-            catch
+            catch err
                 % Connection broken => reset
                 obj.connected = false;
                 disp('WARNING: OHB TCP server disconnected!')
@@ -80,6 +103,26 @@ classdef OHBControl < handle
 
             obj.sendCmd('sample', sampletime);
         end
+
+        %% Open Main Data file
+        function openLogFile(obj)
+            obj.sendCmd('openLogFile');
+        end
+
+        %% Read Last Sample
+        function lastSampleStr = readLastSample(obj)
+            arguments
+                obj
+            end
+
+            result = obj.sendCmd('readLastSample',[],true);
+            
+            if ~isempty(result)
+                lastSampleStr = result(1);
+            else
+                lastSampleStr = '';
+            end
+        end
         
         %% Record a new time series
         function timeSeries(obj, sampletime)
@@ -92,16 +135,25 @@ classdef OHBControl < handle
         end
         
         %% Set a new yaw value and start movement
-        function moveYaw(obj, yaw, speed, acceleration, deceleration)
+        function moveYaw(obj, yaw, speed, acceleration, deceleration,opts)
             arguments
                 obj
                 yaw (1,1) {mustBeNumeric}
                 speed (1,1) {mustBeInRange(speed,0,2,'exclude-lower')} = 0.5
                 acceleration (1,1) {mustBeInRange(acceleration,0,2,'exclude-lower')} = 0.5
                 deceleration (1,1) {mustBeInRange(deceleration,0,2,'exclude-lower')} = 0.5
+                opts.blocking logical = false; 
             end
 
             obj.sendCmd('moveYaw', [yaw, speed, acceleration, deceleration]);
+            if opts.blocking
+                pause(0.5);
+                curYaw = obj.readYaw();
+                while isnan(curYaw) || abs(curYaw-yaw)>0.1
+                    pause(1);
+                    curYaw = obj.readYaw();
+                end
+            end
         end
         
         %% Read the yaw value from the OHB UI
@@ -118,18 +170,66 @@ classdef OHBControl < handle
                 yaw = NaN;
             end
         end
+
+        function StartAll(obj)
+            obj.sendCmd('StartAll');
+        end
+        function StopAll(obj)
+            obj.sendCmd('StopAll');
+        end
+        function PauseWindSpeedControl(obj)
+            obj.sendCmd('PauseWindSpeedControl');
+        end
+
+        %% Set a WindSpeed value and start movement
+        function SetWindSpeed(obj, Windspeed ,opts)
+            arguments
+                obj
+                Windspeed (1,1) {mustBeNumeric}
+                opts.blocking logical = false; 
+            end
+            if Windspeed ~= 0
+                % get Current Windspeed
+                speed = obj.readWindSpeed();
+                while isnan(speed)
+                    pause(1)
+                    speed = obj.readWindSpeed();
+                end
+                if abs(Windspeed-speed)>15
+                    error('Cant change windspeed by more than 15 m/s in one go')
+                end
+            end
+            obj.sendCmd('SetWindSpeed', Windspeed);
+            if opts.blocking
+                pause(0.5);
+                speed = obj.readWindSpeed();
+                while isnan(speed) || abs(speed-Windspeed)>0.25
+                    pause(1);
+                    speed = obj.readWindSpeed();
+                end
+            end
+        end
         
        	%% Set a new incidence value and start movement
-        function moveIncidence(obj, incidence, speed, acceleration, deceleration)
+        function moveIncidence(obj, incidence, speed, acceleration, deceleration,opts)
             arguments
                 obj
                 incidence (1,1) {mustBeNumeric}
                 speed (1,1) {mustBeInRange(speed,0,2,'exclude-lower')} = 0.5
                 acceleration (1,1) {mustBeInRange(acceleration,0,2,'exclude-lower')} = 0.5
                 deceleration (1,1) {mustBeInRange(deceleration,0,2,'exclude-lower')} = 0.5
+                opts.blocking logical = false; 
             end
 
             obj.sendCmd('moveIncidence', [incidence, speed, acceleration, deceleration]);
+            if opts.blocking
+                pause(0.5);
+                curInc = obj.readIncidence();
+                while isnan(curInc) || abs(curInc-incidence)>0.1
+                    pause(1);
+                    curInc = obj.readIncidence();
+                end
+            end
         end
         
         %% Read the incidence value from the OHB UI
