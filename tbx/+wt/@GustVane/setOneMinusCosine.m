f27673cd0588b1d577a9d8a78fa13302a373ae62
Fintan Healy <qe19391@brisotl.ac.uk>
2022-11-07 15:31:21 +0000

    added repatition to setting gust values

--- tbx/+wt/@GustVane/setOneMinusCosine.m
+++ tbx/+wt/@GustVane/setOneMinusCosine.m
@@ -1,9 +1,18 @@
-function setOneMinusCosine(obj,amplitude,freq,inverted)
+function setOneMinusCosine(obj,amplitude,freq,inverted,opts)
+arguments
+obj
+amplitude
+freq
+inverted
+opts.repeats = 2;
+end
 %SETRANDOMGUST Summary of this function goes here
 %   Detailed explanation goes here
+for i = 1:opts.repeats
 obj.setAmplitude(amplitude,wt.GustMode.OneMinusCosine);
 obj.setFrequency(freq);
 obj.setInverted(inverted);
 obj.setMode(wt.GustMode.OneMinusCosine);
+end
 end
 
--- tbx/+wt/@GustVane/setSineGust.m
+++ tbx/+wt/@GustVane/setSineGust.m
@@ -1,8 +1,16 @@
-function setSineGust(obj,amplitude,freq)
+function setSineGust(obj,amplitude,freq,opts)
+arguments
+obj
+amplitude
+freq
+opts.repeats = 2;
+end
 %SETRANDOMGUST Summary of this function goes here
 %   Detailed explanation goes here
+for i = 1:opts.repeats
 obj.setAmplitude(amplitude,wt.GustMode.Sine);
 obj.setFrequency(freq);
 obj.setMode(wt.GustMode.Sine);
+end
 end
 
